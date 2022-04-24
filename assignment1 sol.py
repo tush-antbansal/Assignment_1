85087f7b43cbe13d40d8c56a65c5dae254889fc3
tush-antbansal <101082652+tush-antbansal@users.noreply.github.com>
2022-04-24 23:25:18 +0530

    Add files via upload

---
+++ assignment1 sol.py
@@ -0,0 +1,30 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: C:/Users/lenovo/Desktop/assignment 1/assignment1.py =========
+============ QUESTION 1 ===========
+Enter First Number: 21
+Enter Second number: 27
+Enter Third number: 24
+The average is : 24.0
+============ QUESTION 2 ===========
+Please enter your income: 2000000
+Please enter number of dependents: 4
+Total Payable tax is:  395600.0
+============ QUESTION 3 ===========
+Enter your SID: 21102084
+Enter your name: Tushant Bansal
+Enter your Gender(M/F/U): M
+Enter your Course: Civil
+Enter your cgpa: 8.5
+[21102084, 'Tushant Bansal', 'M', 'Civil', 8.5]
+============ QUESTION 4 ===========
+Marks of 1st student : 54
+Marks of 2nd student : 45
+Marks of 3rd student : 65
+Marks of 4th student : 44
+Marks of 5th student : 55
+The list of marks of students is :  [54, 45, 65, 44, 55]
+============ QUESTION 5 ===========
+Updated list:  ['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'purple', 'Yellow']
